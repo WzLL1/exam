525ebb1d389df5e735df791f502e2ef4db19d147
WzLL1 <81236386+WzLL1@users.noreply.github.com>
2021-03-23 17:05:42 +0500

    exam task
    var 8

---
+++ RandomGenerator/RandomDistributionUnitTestProject/T3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RandomGenerator;
+
+namespace RandomDistributionUnitTestProject
+{
+    public class T3
+    {
+        [FromDistribution(typeof(NormalDistribution), 1, 2, 3)]
+        public double WrongDistributionArguments { get; set; }
+    }
+}
